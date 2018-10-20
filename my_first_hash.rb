27d71a63f0aaebeabee7ca64167a92e606c080c6
Paul Clark <clark.paul123@gmail.com>
2018-10-20 11:54:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,11 +4,11 @@ end
 
 
 def shipping_manifest
-  the_manifest = {
-    "whale bone corsets" => 5, 
-    "porcelain vases" => 2, 
-    "oil paintings" => 3
-  } 
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }          
 end
 
 def retrieval
@@ -19,6 +19,7 @@ def retrieval
                  }
                  
   shipping_manifest["oil paintings"]
+  
 end
 
 def adding
